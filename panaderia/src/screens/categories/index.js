148bd623992c3080bbf5465b5f08969642a00d5b
Keiver Fajardo <ing.kfajardo@gmail.com>
2022-01-25 18:58:20 -0300

    h

---
+++ panaderia/src/screens/categories/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+
+import styles from './styles';
+
+const Categories = () => {
+    return (
+        <SafeAreaView style={styles.screen}>
+            <View style={styles.screen}>
+                <Text>
+                    Categories
+                    
+                </Text>
+            </View>
+        </SafeAreaView>
+    );
+};
+
+
+
+export default Categories
---
+++ panaderia/src/screens/products/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+
+import styles from './styles'
+
+const Products = () => {
+    return (
+        <SafeAreaView style={styles.screen}>
+            <View style={styles.screen}>
+                <Text>
+                    Products
+                </Text>
+            </View>
+        </SafeAreaView>
+    );
+};
+
+
+export default Products
